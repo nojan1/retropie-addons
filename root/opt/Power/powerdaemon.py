39e37a835b60997ff26b1293d576af966d5d1afa
Niklas Hedlund <nojan1989@gmail.com>
2016-03-03 22:06:09 +0100

    Syntax fixes

--- root/opt/Power/powerdaemon.py
+++ root/opt/Power/powerdaemon.py
@@ -4,14 +4,14 @@ from time import sleep
 PIE_SENSE = 18
 PIE_SIGNAL = 16
 
-run = true
+run = True
 
 GPIO.setmode(GPIO.BCM)  
 GPIO.setup(PIE_SIGNAL, GPIO.OUT)  
 GPIO.setup(PIE_SENSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def PieOFF():
-	run = false
+	run = False
 
 GPIO.add_event_detect(PIE_SENSE, GPIO.FALLING, callback=PieOFF, bouncetime=300)    
 
