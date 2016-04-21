db9f4e15646b7f536ad576dcca7f217a3d0316fa
Niklas Hedlund <nojan1989@gmail.com>
2016-04-21 19:56:05 +0200

    Moved to loop based button read

--- src/power-module/RetroPie/RetroPie/RetroPie.c
+++ src/power-module/RetroPie/RetroPie/RetroPie.c
@@ -11,67 +11,29 @@
 #include "fan.h"
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
+const int SHORT_CLICK = 3;
+const int LONG_CLICK = 15;
+const int SHUTDOWN_WAIT = 10;
+
 unsigned char waitCounter = 0;
+unsigned char ledState = 1;
 
-ISR (ADC_vect) 
+void UpdateLeds()
 {
-	//ADC Conversion completed - temp reading in ADCL
-	HandleFan(ADCL);
-}
-
-ISR (PCINT1_vect)
-{
-	cli();
-	//PWR Switch or raspberry pie sense
-	_delay_ms(50);
-
-	if( PINC & (1 << PWR_SWITCH) ){
-		//PWR Switch clicked
-		switch(GetState()){
-			case OFF:
-				SetState(BOOTING);
-				_delay_ms(100);
-			break;
-			case RUNNING:
-				SetState(SHUTTING_DOWN);
-			break;
-		}
-		
-		return;
+	PORTD = 0xff;
+	PORTD &= ~ledState;
+	
+	ledState = ledState << 1;
+	if(ledState > 8)
+	{
+		ledState = 1;
 	}
-	
-	//PIE_SENSE went high
-	if((PINC & (~(1 << PIE_SENSE))) && waitCounter > 20 && GetState() == BOOTING){
-		waitCounter = 0;
-		SetState(RUNNING);
-	}
-	
-	////PIE_SENSE went low
-	//if( !(PINC & (~(1 << PIE_SENSE))) && waitCounter > 10 && GetState() == SHUTTING_DOWN){
-		//waitCounter = 0;
-		////Raspberry switched off
-		//SetState(OFF);
-	//}
-	
-	sei();
 }
 
 int main(void)
 {
-	//Select channel 4
-	ADMUX |= (1 << MUX2);
-	//Enable ADC, Start Conversion, Enable interrupt, Prescale = 64
-	ADCSRA = 0b11101110;
-	//Set free running ADC	
-	ADCSRB |= (0 << 3);
-
-	//Fast PWM mode, set OC0A at bottom clear at match, Prescale = 64
-	TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
-	TCCR0B |= (1 << WGM02) | (1 << CS01) | (1 << CS00);
-
 	//Data pins	
 	DDRC = (1 << PIE_SIGNAL) |
 			(0 << PIE_SENSE) |
@@ -84,46 +46,76 @@ int main(void)
 	//Turn of LED's
 	//PORTD |= (0b1111 << LED1);
 	PORTD = 0xff;
-	
-	//Enable PCINT[14:8] interrupt
-	PCICR |= (1 << PCIE1);
-	//Enable for PCINT9 and 11
-	PCMSK1 |= (1 << PCINT11) | (1 << PCINT9);
 
-	
 	SetState(OFF);
-	
-	sei();	
-	
-	unsigned char ledState = 1;
+
+	unsigned char powerbuttonDownCounter = 0;
 	
     while(1)
     {
-        switch(currentState){
-			case BOOTING:
-			case SHUTTING_DOWN:
-				PORTD = 0xff;
-				PORTD &= ~ledState;
+		//Check power button
+		powerbuttonDownCounter = 0;
+		while(PINC & (1 << PWR_SWITCH))
+		{
+			powerbuttonDownCounter++;
+			_delay_ms(20);
+		}
 			
-				ledState = ledState << 1;
-				if(ledState > 8){
-					ledState = 1;
+		if(powerbuttonDownCounter >= LONG_CLICK)
+		{
+			//Reset
+			SetState(OFF);
+			waitCounter = 0;
+		}
+		else if(powerbuttonDownCounter >= SHORT_CLICK)
+		{
+			
+			switch(GetState())
+			{
+				case OFF:
+					SetState(BOOTING);
+					_delay_ms(100);
+					break;
+				case RUNNING:
+					SetState(SHUTTING_DOWN);
+					break;
+			}
+			
+			waitCounter = 0;
+		}
+		
+        switch(currentState)
+		{
+			case RUNNING:
+				if( !(PINC & (1 << PIE_SENSE)))
+				{
+					SetState(SHUTTING_DOWN);
 				}
+				break;
+			case BOOTING:
+				UpdateLeds();
 			
-				_delay_ms(250);
-				
-				waitCounter++;
-
-				//Check if PIE_Sense is low
-				if( !(PINC & (1 << PIE_SENSE)) && waitCounter > 10 && GetState() == SHUTTING_DOWN){
+				if((PINC & (~(1 << PIE_SENSE))) && waitCounter > 20)
+				{
 					waitCounter = 0;
-					//Raspberry switched off
+					SetState(RUNNING);
+				}
+				break;
+			case SHUTTING_DOWN:
+				UpdateLeds();
+				waitCounter++;		
+
+				if( !(PINC & (1 << PIE_SENSE)) && waitCounter > SHUTDOWN_WAIT)
+				{
+					waitCounter = 0;
+
 					SetState(OFF);
 				}
 
 			break;	
 			
 		}
-	
+		
+		_delay_ms(250);
     }
 }
